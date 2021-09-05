fef64c3764b6d636bf189aa9168e01eb1b9e8cd0
orphee-celui-qui-ne-sait-rien <79347623+orphee-celui-qui-ne-sait-rien@users.noreply.github.com>
2021-09-04 22:14:34 -0500

    Update ex_7.py

--- Scripts/ex_7.py
+++ Scripts/ex_7.py
@@ -5,13 +5,13 @@ def generate(p1):
     # change this so that it generates 10000 random zeros and ones
     # where the probability of one is p1
     seq = np.random.choice([0, 1], p=[1 - p1, p1], size=10000)
-    return list(seq)
+    return seq
 
 
 def count(seq):
     c = 0
-    for i in range(len(seq[:-5])):
-        if seq[i:i + 5] == [1, 1, 1, 1, 1]:
+    for i in range(len(seq[:-4])):
+        if list(seq[i:i + 5]) == [1, 1, 1, 1, 1]:
             c += 1
     return c
 
